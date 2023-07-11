dc88ff84cc5d1e6c8ecec39f65c8e396331c400c
Hauke <runge@ridon.de>
2023-07-11 12:05:40 +0200

    new entity

--- src/main/java/com/ridonit/alm/model/CloudRequirement.java
+++ src/main/java/com/ridonit/alm/model/CloudRequirement.java
@@ -1,45 +1,114 @@
-package com.ridonit.alm.payload;
+package com.ridonit.alm.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
-public class CloudRequirementDto {
+import javax.persistence.*;
+import java.io.Serializable;
 
-    @JsonIgnore
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CloudRequirement implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long requirementId;
 
-    @JsonIgnore
+    @Version
     private Integer version;
 
+    @Column
     private String id;
+
+    @Column
     private String projectId;
+
+    @Column
     private String title;
+
+    @Column
     private String type;
+
+    @Column
     private String status;
+
+    @Column
     private String externalId;
+
+    @Column
     private String startDate;
+
+    @Column
     private String dueDate;
+
+    @Column
     private Long priorityId;
+
+    @Column
     private String assigneeId;
+
+    @Column
     private String assigneeName;
+
+    @Column
     private String timeboxName;
+
+    @Column
     private String timeboxStartDate;
+
+    @Column
     private String timeboxEndDate;
+
+    @Column
     private String parentId;
+
+    @Column
     private String subStatus;
+
+    @Column
     private String approvalState;
+
+    @Column
     private String description;
+
+    @Column
     private String scopeId;
+
+    @Column
     private String scopeName;
+
+    @Column
     private String assignedRoleId;
+
+    @Column
     private String assignedRoleName;
+
+    @Column
     private String storyPoints;
+
+    @Column
     private Boolean obsolete;
+
+    @Column
     private String workstream;
+
+    @Column
     private String deliverableId;
+
+    @Column
     private String createdTimestamp;
+
+    @Column
     private String lastChangedTimestamp;
+
+    @Column
     private Integer effort;
+
+    @Column
     private String involvedParties;
 }
